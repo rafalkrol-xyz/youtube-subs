29123095f12de528ba71bc7cfbe5f9bee21cd9ac
Rafal Krol <ameotoko1+github@gmail.com>
2023-01-07 20:24:49 +0100

    Docs: update descriptions

--- src/video.py
+++ src/video.py
@@ -5,10 +5,14 @@ import subprocess
 
 
 def download(url: str) -> str:
-    """Download a YouTube video in an mp4 format using the provided URL.
+    """
+    Download a YouTube video in an mp4 format using the provided URL.
 
     :param str url:
         The URL to a YouTube video, e.g.: 'https://www.youtube.com/watch?v=K2tA-R3v1GI'
+    
+    :return: str
+        The default name of the downloaded video file ('video.mp4').
     """
     yt = pytube.YouTube(url=url)
     video_path = (yt.streams.filter(progressive=True, file_extension='mp4')
@@ -24,7 +28,15 @@ def download(url: str) -> str:
 
 
 def info(video_filepath: str) -> tuple[int, int, int]:
-    """Returns the number of channels, bit rate, and sample rate of the provided video"""
+    """
+    Returns the number of channels, bit rate, and sample rate of the provided video.
+
+    :param video_filepath: str
+        The filepath to the video file.
+
+    :return: tuple[int, int, int]
+        A tuple containing the number of channels, bit rate, and sample rate of the video.
+    """
 
     video_data = mediainfo(video_filepath)
 
@@ -36,6 +48,21 @@ def video_to_audio(video_filepath: str,
                    video_channels: int,
                    video_bit_rate: int,
                    video_sample_rate: int) -> None:
+    """
+    Convert a video file to an audio file using ffmpeg.
+
+    :param video_filepath: str
+        The filepath to the video file.
+    :param audio_filename: str
+        The name of the output audio file.
+    :param video_channels: int
+        The number of channels in the video file.
+    :param video_bit_rate: int
+        The bit rate of the video file.
+    :param video_sample_rate: int
+        The sample rate of the video file.
+    """
+
     command = (
         f"ffmpeg -i {video_filepath} "
         f"-b:a {video_bit_rate} "
